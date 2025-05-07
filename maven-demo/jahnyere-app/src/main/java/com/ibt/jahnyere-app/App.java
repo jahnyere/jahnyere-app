b20ac9ec066d98d120b26d9716c757999854e35c
Jahnyere <jahnyjon2016@gmail.com>
2025-05-07 14:20:33 +0100

    maven demo

---
+++ maven-demo/jahnyere-app/src/main/java/com/ibt/jahnyere-app/App.java
@@ -0,0 +1,8 @@
+package com.ibt.jahnyere_app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! this is Jahnyere");
+    }
+}
+
---
+++ maven-demo/jahnyere-app/src/test/java/com/ibt/jahnyere-app/AppTest.java
@@ -0,0 +1,12 @@
+package com.ibt.jahnyere_app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true); // Simple test assertion
+    }
+}
+
